334ddb51da739b63805232b4529b8a8a08e6a56a
msmithivas <msmithivas@gmail.com>
2023-03-22 20:01:20 +0000

    Ran rails db:migrate Post migration files created. Inserted boilerplate into application.html.erb template. No sample data installed yet.

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: likes
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  fan_id     :integer
+#  photo_id   :integer
+#  user_id    :integer
+#
 class Like < ApplicationRecord
 
   validates(:user_id, { :presence => true })
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                             :integer          not null, primary key
+#  comments_count                 :integer
+#  email                          :string
+#  own_photos_count               :integer
+#  password_digest                :string
+#  private                        :boolean
+#  received_follow_requests_count :integer
+#  sent_follow_requests_count     :integer
+#  username                       :string
+#  created_at                     :datetime         not null
+#  updated_at                     :datetime         not null
+#
 class User < ApplicationRecord
   validates :email, :uniqueness => { :case_sensitive => false }
   validates :email, :presence => true
