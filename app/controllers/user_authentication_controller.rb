692ef72d63316990c9a2a5b655b8762bdc172e7d
msmithivas <msmithivas@gmail.com>
2023-04-04 16:03:08 +0000

    User likes page Still working on getting a user's likes to appear

--- app/controllers/user_authentication_controller.rb
+++ app/controllers/user_authentication_controller.rb
@@ -1,6 +1,6 @@
 class UserAuthenticationController < ApplicationController
   # Uncomment line 3 in this file and line 5 in ApplicationController if you want to force users to sign in before any other actions.
-  # skip_before_action(:force_user_sign_in, { :only => [:sign_up_form, :create, :sign_in_form, :create_cookie] })
+  skip_before_action(:force_user_sign_in, { :only => [:sign_up_form, :create, :sign_in_form, :create_cookie] })
 
   def index
   @list_of_all_users = User.all.order(:username)
@@ -24,6 +24,15 @@ class UserAuthenticationController < ApplicationController
         render({ :template => "user_authentication/feed.html.erb" })
     
       end
+
+      def show_likes
+    
+        @the_user = @current_user
+        @the_user_likes = Like.where({ :id => @the_user.id })
+      
+          render({ :template => "likes/show.html.erb" })
+      
+        end
   
   def sign_in_form
     render({ :template => "user_authentication/sign_in.html.erb" })
